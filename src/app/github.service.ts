31b34d52ffe25929d7fa5e5be066fe740e6b30ab
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-05-10 19:55:53 -0300

    1 file modified

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,20 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 interface Repo {
-  id: number,
-  full_name: string
+  id: number;
+  full_name: string;
 }
 
 @Injectable()
 export class GithubService {
   repos: Array<Repo> = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   update() {
-    this.http.get<Array<Repo>>('https://api.github.com/users/santana/repos')
-    .subscribe(data => {
-      this.repos = data;
-    });
+    this.http
+      .get<Array<Repo>>('https://api.github.com/users/santana/repos')
+      .subscribe((data) => {
+        this.repos = data;
+      });
   }
 }

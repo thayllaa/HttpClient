263a7f0762e26acade41c6ed2c070b4b9c5aa76e
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-05-10 20:33:52 -0300

    1 file modified

--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -22,14 +22,30 @@ export class BitcoinService {
   currentPrice: Response;
   list: Array<Response> = [];
 
+  private timer: any;
+  private counter = 0;
+
   constructor(private http: HttpClient) {}
 
   update() {
-    this.http
-      .get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
-      .subscribe((data) => {
-        this.currentPrice = data;
-        this.list.push(data);
-      });
+    this.http.get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+    .subscribe(data => {
+      this.currentPrice = data;
+      this.list.push(data);
+    });
+  }
+
+  myTimer(ms: number) {
+    if (!this.timer) {
+      this.counter = 60;
+      this.timer = this.counter - 1;
+      if (this.counter == 60) {
+        return this.update();
+      }
+    }
+  }
+
+  getTimer() {
+    return this.counter;
   }
 }
